bbc3f99cf5e8f67a1306a81a3ffc83c1a327c17c
antoine-bdr <32259439+antoine-bdr@users.noreply.github.com>
2024-03-05 17:17:49 +0100

    Delete Code php/SitePPE/sitePPE directory

--- Code php/SitePPE/sitePPE/inc/menu.php
+++
@@ -1,7 +0,0 @@
-<nav class="navbar navbar-inverse">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <a class="navbar-brand" style="color:orange">Bienvenue sur AGRUR</a>
-        </div>
-    </div>
-</nav>
